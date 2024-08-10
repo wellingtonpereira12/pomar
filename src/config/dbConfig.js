fea4ec7a0c1ec008abf2ee5284cfc0b39532e6ae
Wellington Pereira <lleco12@gmail.com>
2024-08-10 19:19:26 -0300

    banco

--- src/config/dbConfig.js
+++ src/config/dbConfig.js
@@ -4,11 +4,11 @@ const { Pool } = require('pg');
 
 // Configuração do pool de conexões
 const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT || 5432,
+  user: 'pomardb',
+  host: 'pomardb.cvkig0w8kioa.us-east-1.rds.amazonaws.com',
+  database: 'postgres',
+  password: 'pomardb123',
+  port: 5432,
   ssl: {
     rejectUnauthorized: false  // Isso desativa a verificação do certificado; use com cuidado.
   }
